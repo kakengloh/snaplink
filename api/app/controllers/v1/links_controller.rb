18be6e67a3623e2266b3481c557adeb952893000
kakeng <kakengloh@gmail.com>
2023-03-26 12:58:20 +0800

    Create link shortener and call it from links controller

--- api/app/controllers/v1/links_controller.rb
+++ api/app/controllers/v1/links_controller.rb
@@ -1,3 +1,9 @@
 class V1::LinksController < ApplicationController
-  def create; end
+  def create
+    params.require(:target_url)
+
+    link = LinkShortener.new(params[:target_url]).execute
+
+    render(json: link)
+  end
 end
---
+++ api/app/services/link_shortener.rb
@@ -0,0 +1,16 @@
+class LinkShortener
+  def initialize(target_url)
+    @target_url = target_url
+  end
+
+  def execute
+    slug = SlugGenerator.new.execute
+
+    Link.create(
+      title: SiteTitleFetcher.new(@target_url).execute,
+      slug: slug,
+      short_url: "#{Rails.application.config.origin}/#{slug}",
+      target_url: @target_url
+    )
+  end
+end
