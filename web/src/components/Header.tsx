7ebaca63630aadf31d631ad602a91daa135e6d90
kakeng <kakengloh@gmail.com>
2023-03-26 17:43:44 +0800

    Add heading and description

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <nav className="bg-[#05010D] px-96 py-6">
+    <nav className="bg-[#05010D] px-96 h-28 flex items-center">
       <span className="text-white font-bold text-3xl">snaplink</span>
     </nav>
   );
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -14,7 +14,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className="h-screen w-screen bg-gradient-to-b from-[#05010D] to-[#21014A]"></main>
+      <main className="h-[calc(100vh-7rem)] w-screen bg-gradient-to-b from-[#05010D] to-[#21014A] text-white">
+        <div className="flex justify-around items-center pt-48">
+          <div className="space-y-5">
+            <h1 className="font-extrabold text-6xl">
+              Short links, big impact.
+            </h1>
+            <p className="text-[#937F8B] text-xl">
+              Snaplink the link shortener that simplifies your links, amplifies
+              your message and builds trust with your audience.
+            </p>
+          </div>
+          <div></div>
+        </div>
+      </main>
     </>
   );
 }
