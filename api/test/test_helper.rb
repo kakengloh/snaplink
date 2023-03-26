90acd05795ace625abe89eef6e1258fcfa8e8ba4
kakeng <kakengloh@gmail.com>
2023-03-26 17:02:33 +0800

    Exclude jobs from coverage

--- api/test/test_helper.rb
+++ api/test/test_helper.rb
@@ -9,6 +9,7 @@ require 'rails/test_help'
 SimpleCov.start('rails') do
   add_filter('/app/channels/')
   add_filter('/app/mailers/')
+  add_filter('/app/jobs/')
 end
 
 class ActiveSupport::TestCase
