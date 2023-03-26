3a92a5268a26aa56a0acb4003d32125a7e8cf1ae
kakeng <kakengloh@gmail.com>
2023-03-26 13:53:27 +0800

    Add fixtures and model tests

--- api/test/models/link_test.rb
+++ api/test/models/link_test.rb
@@ -1,7 +1,31 @@
-require "test_helper"
+require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should create link' do
+    Link.create!(
+      slug: 'A7Y9BN',
+      target_url: 'https://www.coingecko.com',
+      short_url: 'http://localhost:3000/A7Y9BN',
+      title: 'Coingecko'
+    )
+  end
+
+  test 'should create link even if title is nil' do
+    Link.create!(
+      slug: 'A7Y9BN',
+      target_url: 'https://www.coingecko.com',
+      short_url: 'http://localhost:3000/A7Y9BN',
+      title: nil
+    )
+  end
+
+  test 'should not create link if slug is absent' do
+    assert_raise(ActiveRecord::RecordInvalid) do
+      Link.create!(
+        target_url: 'https://www.coingecko.com',
+        short_url: 'http://localhost:3000/A7Y9BN',
+        title: 'Coingecko'
+      )
+    end
+  end
 end
--- api/test/models/link_visit_test.rb
+++ api/test/models/link_visit_test.rb
@@ -1,7 +1,26 @@
-require "test_helper"
+require 'test_helper'
 
 class LinkVisitTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should create link visit' do
+    LinkVisit.create!(
+      link_id: 1,
+      user_agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36',
+      referrer: 'https://www.google.com/search?q=coingecko',
+      ip_address: '185.84.24.13',
+      country: 'Malaysia',
+      city: 'Kuala Lumpur'
+    )
+  end
+
+  test 'should not create link visit without link ID' do
+    assert_raise(ActiveRecord::RecordInvalid) do
+      LinkVisit.create!(
+        user_agent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36',
+        referrer: 'https://www.google.com/search?q=coingecko',
+        ip_address: '185.84.24.13',
+        country: 'Malaysia',
+        city: 'Kuala Lumpur'
+      )
+    end
+  end
 end
