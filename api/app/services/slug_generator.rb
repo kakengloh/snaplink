a3e54ccf0489a8f56d2aa4af221d9434d145d2ad
kakeng <kakengloh@gmail.com>
2023-03-26 12:41:55 +0800

    Install hashids gem and create slug generator

---
+++ api/app/services/slug_generator.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Service to generate unique slug by hashing PostgreSQL sequence
+class SlugGenerator
+  HASH_SALT = 'SHORT_LINK_SLUGS'
+
+  def execute
+    result = ActiveRecord::Base.connection.execute("SELECT nextval('slug_seq')")
+    HashIds.new(HASH_SALT, 6).encode(result.values)
+  end
+end
