4e5eb4d603d8c9f79aa005d3de3bf1c22c73e525
kakeng <kakengloh@gmail.com>
2023-03-26 13:12:37 +0800

    Add resource not found exception handler

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found_exception
+
+  private
+
+  def handle_not_found_exception
+    render(
+      json: {
+        error: {
+          code: 'NOT_FOUND',
+          message: 'The requested resource was not found.'
+        }
+      },
+      status: :not_found
+    )
+  end
 end
